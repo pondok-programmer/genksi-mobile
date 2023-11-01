28ce6932b8551cc80fb1875c0461ddd62af3ac67
Radiant-F <radiantfadilah0@gmail.com>
2023-11-01 12:53:56 +0700

    import home & lib demo

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,5 +2,7 @@ import Login from './Login/Login';
 import OnBoarding from './OnBoarding/OnBoarding';
 import Register from './Register/Register';
 import SplashScreen from './SplashScreen/SplashScreen';
+import Home from './Home/Home';
+import LibDemo from './__test__/LibDemo.test';
 
-export {Login, Register, SplashScreen, OnBoarding};
+export {Login, Register, SplashScreen, OnBoarding, Home, LibDemo};
