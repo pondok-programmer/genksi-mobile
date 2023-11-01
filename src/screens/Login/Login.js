9e6db7fb2e5d1501090bfaf27d57aecaca9741fd
Radiant-F <radiantfadilah0@gmail.com>
2023-11-01 12:53:28 +0700

    slicing

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,21 +1,79 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import {Background} from '../../components';
+import {StyleSheet, Text, TouchableNativeFeedback, View} from 'react-native';
+import React, {useState} from 'react';
+import {Background, Gap} from '../../components';
 import {FormInput} from '../../features/Auth';
 
-export default function Login() {
+export default function Login({navigation}) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function submitLogin() {
+    console.log({email, password});
+  }
+
   return (
     <View style={{flex: 1}}>
       <Background />
       <View style={styles.container}>
         <Text style={styles.textTitle}>Login</Text>
-        <FormInput />
+        <View style={{height: 20}} />
+        <FormInput
+          onChangeText={setEmail}
+          placeholder="contoh@email.com"
+          autoCapitalize={'none'}
+        />
+        <View style={{height: 15}} />
+        <FormInput
+          onChangeText={setPassword}
+          password
+          iconName="lock"
+          autoCapitalize={'none'}
+        />
+        <Gap height={20} />
+        <TouchableNativeFeedback onPress={submitLogin} useForeground>
+          <View style={styles.btnSubmit}>
+            <Text style={styles.textBtnSubmit}>Masuk</Text>
+          </View>
+        </TouchableNativeFeedback>
+        <Gap height={10} />
+        <TouchableNativeFeedback
+          onPress={() => navigation.navigate('Register')}
+          useForeground>
+          <View style={styles.btnRegister}>
+            <Text style={styles.textBtnSubmit}>Daftar</Text>
+          </View>
+        </TouchableNativeFeedback>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  textBtnSubmit: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  btnRegister: {
+    backgroundColor: '#006bd6',
+    width: 180,
+    height: 50,
+    borderRadius: 50 / 2,
+    elevation: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  btnSubmit: {
+    backgroundColor: 'dodgerblue',
+    width: 275,
+    height: 50,
+    borderRadius: 50 / 2,
+    elevation: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
   textTitle: {
     color: 'black',
     fontSize: 25,
@@ -25,5 +83,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
+    width: '100%',
+    maxWidth: 480,
+    alignSelf: 'center',
   },
 });
--- src/screens/Register/Register.js
+++ src/screens/Register/Register.js
@@ -1,12 +1,99 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, TouchableNativeFeedback, View} from 'react-native';
+import React, {useState} from 'react';
+import {Background, Gap} from '../../components';
+import {FormInput} from '../../features/Auth';
 
-export default function Register() {
+export default function Register({navigation}) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function submitRegister() {
+    console.log({email, password});
+    navigation.replace('Home');
+  }
+
   return (
-    <View>
-      <Text>Register</Text>
+    <View style={{flex: 1}}>
+      <Background />
+      <View style={styles.container}>
+        <Text style={styles.textTitle}>Register</Text>
+        <View style={{height: 20}} />
+        <FormInput
+          onChangeText={setName}
+          placeholder="Masukan nama"
+          autoCapitalize={'words'}
+          iconName="account"
+        />
+        <View style={{height: 15}} />
+        <FormInput
+          onChangeText={setEmail}
+          placeholder="contoh@email.com"
+          autoCapitalize={'none'}
+        />
+        <View style={{height: 15}} />
+        <FormInput
+          onChangeText={setPassword}
+          password
+          iconName="lock"
+          autoCapitalize={'none'}
+        />
+        <Gap height={20} />
+        <TouchableNativeFeedback onPress={submitRegister} useForeground>
+          <View style={styles.btnSubmit}>
+            <Text style={styles.textBtnSubmit}>Daftar</Text>
+          </View>
+        </TouchableNativeFeedback>
+        <Gap height={10} />
+        <TouchableNativeFeedback
+          onPress={() => navigation.goBack()}
+          useForeground>
+          <View style={styles.btnRegister}>
+            <Text style={styles.textBtnSubmit}>Kembali</Text>
+          </View>
+        </TouchableNativeFeedback>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textBtnSubmit: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  btnRegister: {
+    backgroundColor: '#006bd6',
+    width: 180,
+    height: 50,
+    borderRadius: 50 / 2,
+    elevation: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  btnSubmit: {
+    backgroundColor: 'dodgerblue',
+    width: 275,
+    height: 50,
+    borderRadius: 50 / 2,
+    elevation: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  textTitle: {
+    color: 'black',
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    width: '100%',
+    maxWidth: 480,
+    alignSelf: 'center',
+  },
+});
