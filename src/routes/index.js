b0c41bb44a1ca1520864b72804e80cc881ac53f5
Radiant-F <radiantfadilah0@gmail.com>
2023-11-01 12:53:43 +0700

    update stack screens

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import RNBootSplash from 'react-native-bootsplash';
 
-import {Login, OnBoarding, Register, SplashScreen} from '../screens';
+import {
+  Home,
+  LibDemo,
+  Login,
+  OnBoarding,
+  Register,
+  SplashScreen,
+} from '../screens';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,11 +22,14 @@ export default function Navigator() {
           headerShown: false,
           statusBarColor: 'transparent',
           statusBarTranslucent: true,
-        }}>
+        }}
+        initialRouteName="LibDemo">
         <Stack.Screen name="SplashScreen" component={SplashScreen} />
         <Stack.Screen name="OnBoarding" component={OnBoarding} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="LibDemo" component={LibDemo} />
       </Stack.Navigator>
     </NavigationContainer>
   );
