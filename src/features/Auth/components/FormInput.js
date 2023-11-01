bf5b7de0c558892bb2ef4567ff3027669612b1f5
Radiant-F <radiantfadilah0@gmail.com>
2023-11-01 12:53:19 +0700

    slicing

--- src/features/Auth/components/FormInput.js
+++ src/features/Auth/components/FormInput.js
@@ -1,12 +1,61 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableNativeFeedback,
+} from 'react-native';
+import React, {useState} from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function FormInput() {
+export default function FormInput({
+  iconName = 'gmail',
+  password,
+  autoCapitalize,
+  placeholder = 'Placeholder',
+  onChangeText,
+}) {
+  const [secureText, setSecureText] = useState(true);
   return (
-    <View>
-      <Text>FormInput</Text>
+    <View style={styles.container}>
+      <Icon name={iconName} color={'black'} size={25} />
+      <TextInput
+        style={{flex: 1, color: 'black', paddingHorizontal: 10}}
+        placeholder={placeholder}
+        placeholderTextColor={'grey'}
+        secureTextEntry={secureText && password}
+        autoCapitalize={autoCapitalize}
+        onChangeText={onChangeText}
+      />
+      {password && (
+        <TouchableNativeFeedback
+          onPress={() => setSecureText(!secureText)}
+          useForeground
+          background={TouchableNativeFeedback.Ripple('#1e90ff45')}>
+          <View style={styles.btnEye}>
+            <Icon name={'eye-outline'} color={'black'} size={25} />
+          </View>
+        </TouchableNativeFeedback>
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  btnEye: {
+    overflow: 'hidden',
+    borderRadius: 50 / 2,
+  },
+  container: {
+    borderWidth: 1,
+    borderColor: 'dodgerblue',
+    height: 50,
+    borderRadius: 50 / 2,
+    backgroundColor: 'white',
+    elevation: 5,
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    width: '80%',
+  },
+});
