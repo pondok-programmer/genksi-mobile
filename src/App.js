1466462e69b54bc3c6d55fcefe8052f93fb4d349
Radiant-F <radiantfadilah0@gmail.com>
2023-11-02 10:59:58 +0700

    move navigation container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {store} from './redux';
 import {Provider} from 'react-redux';
 import Navigator from './routes';
+import {NavigationContainer} from '@react-navigation/native';
+import RNBootSplash from 'react-native-bootsplash';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Navigator />
+      <NavigationContainer onReady={() => RNBootSplash.hide({fade: true})}>
+        <Navigator />
+      </NavigationContainer>
     </Provider>
   );
 }
