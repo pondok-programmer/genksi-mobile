2b07fb1abc8dcd9fc2b74b6aa2c8788807b3d991
Radiant-F <radiantfadilah0@gmail.com>
2023-11-06 13:11:02 +0700

    update import

--- src/screens/index.js
+++ src/screens/index.js
@@ -4,5 +4,6 @@ import Register from './Register/Register';
 import SplashScreen from './SplashScreen/SplashScreen';
 import Home from './Home/Home';
 import LibDemo from './__test__/LibDemo.test';
+import Product from './Product/Product';
 
-export {Login, Register, SplashScreen, OnBoarding, Home, LibDemo};
+export {Login, Register, SplashScreen, OnBoarding, Home, LibDemo, Product};
